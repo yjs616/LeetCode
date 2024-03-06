8468eec0e1225ab0d7dd902589a9ffb4dd34ceb0
YU JEONG SEON <yjs000616@gmail.com>
2024-03-06 10:33:48 +0900

    Time: 161 ms (5.24%), Space: 75.8 MB (5.37%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+#include <map>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(auto it : mp){
+            if(it.second != 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
